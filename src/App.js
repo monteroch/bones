e34ee9a23778e8dac8297e782b900343edff31a3
Christian <christianmontero08@gmail.com>
2020-08-25 16:38:55 -0700

    add Feature A

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Bones App</h2>
-        <h2>Testing from Digital Ocean 1.2</h2>
+        <h2>Testing on pull request from featureA</h2>
       </header>
     </div>
   );
