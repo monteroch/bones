5e588c15ac1d5abd661d77e03a1b902a837645ea
Christian <christianmontero08@gmail.com>
2020-08-25 15:58:01 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Bones App</h2>
-        <h2>Testing from Digital Ocean 1.0</h2>
+        <h2>Testing from Digital Ocean 1.1</h2>
       </header>
     </div>
   );
